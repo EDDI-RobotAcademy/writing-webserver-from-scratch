ad82d243418fb846524d672fad88c10f259a5eb0
hyeyoom <neoul_chw@icloud.com>
2023-02-11 01:53:00 +0900

    chore: 1주차 실습 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,7 @@ repositories {
 dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+    implementation("com.samskivert:jmustache:1.15")
 }
 
 tasks.getByName<Test>("test") {
