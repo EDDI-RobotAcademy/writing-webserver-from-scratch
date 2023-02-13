54833d956685a1e40be20a3fb296b4227a5a0dd7
hyeyoom <neoul_chw@icloud.com>
2023-02-13 23:33:24 +0900

    chore: WAS 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,9 @@ repositories {
 }
 
 dependencies {
+    implementation("org.eclipse.jetty:jetty-server:11.0.13")
+    implementation("org.eclipse.jetty:jetty-servlet:11.0.13")
+
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
     implementation("org.mockito:mockito-core:5.1.1")
