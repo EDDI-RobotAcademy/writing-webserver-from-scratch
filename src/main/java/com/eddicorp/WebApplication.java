e28750f14ba7ae4ecbeacc1ac1217080b4bc07e0
hyeyoom <neoul_chw@icloud.com>
2023-02-26 21:17:19 +0900

    resources 디렉터리에 있는 파일 읽어보기

--- src/main/java/com/eddicorp/WebApplication.java
+++ src/main/java/com/eddicorp/WebApplication.java
@@ -1,8 +1,22 @@
 package com.eddicorp;
 
+import java.io.InputStream;
+
 public class WebApplication {
 
     public static void main(String[] args) throws Exception {
-
+        // resources 디렉터리의 파일 읽어보기
+        try (
+                final InputStream inputStream = WebApplication.class
+                        .getClassLoader()
+                        .getResourceAsStream("pages/index.html")
+        ) {
+            int readCount = 0;
+            final byte[] readBuffer = new byte[8192];
+            while ((readCount = inputStream.read(readBuffer)) != -1) {
+                final String result = new String(readBuffer);
+                System.out.println(result);
+            }
+        }
     }
 }
