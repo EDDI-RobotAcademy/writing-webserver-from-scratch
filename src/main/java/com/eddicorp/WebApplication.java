4d90f00b8063d5570f283670ab9fd4c642ee58a8
hyeyoom <neoul_chw@icloud.com>
2023-02-26 21:26:02 +0900

    file 읽어서 byte array로 반환

--- src/main/java/com/eddicorp/WebApplication.java
+++ src/main/java/com/eddicorp/WebApplication.java
@@ -1,21 +1,36 @@
 package com.eddicorp;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class WebApplication {
 
     public static void main(String[] args) throws Exception {
-        // resources 디렉터리의 파일 읽어보기
-        try (
-                final InputStream inputStream = WebApplication.class
-                        .getClassLoader()
-                        .getResourceAsStream("pages/index.html")
-        ) {
-            int readCount = 0;
-            final byte[] readBuffer = new byte[8192];
-            while ((readCount = inputStream.read(readBuffer)) != -1) {
-                final String result = new String(readBuffer);
-                System.out.println(result);
+        // index.html 브라우저에 띄워보기
+        final ServerSocket serverSocket = new ServerSocket(8080);
+        Socket connection;
+        while ((connection = serverSocket.accept()) != null) {
+            try (
+                    final InputStream inputStream = connection.getInputStream();
+                    final OutputStream outputStream = connection.getOutputStream()
+            ) {
+                try (
+                        final InputStream resourceInputStream = WebApplication.class
+                                .getClassLoader()
+                                .getResourceAsStream("pages/index.html");
+                        final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                ) {
+                    int readCount = 0;
+                    final byte[] readBuffer = new byte[8192];
+                    while ((readCount = resourceInputStream.read(readBuffer)) != -1) {
+                        baos.write(readBuffer, 0, readCount);
+                    }
+                    baos.toByteArray();
+                }
             }
         }
     }
