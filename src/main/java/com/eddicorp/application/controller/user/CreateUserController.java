01d2970f746ddb555e39bf6457c7ccddb2c90dd2
ekankr2 <ekankr2@naver.com>
2023-03-08 00:23:14 +0900

    feat: create body parser

--- src/main/java/com/eddicorp/application/controller/user/CreateUserController.java
+++ src/main/java/com/eddicorp/application/controller/user/CreateUserController.java
@@ -1,10 +1,12 @@
 package com.eddicorp.application.controller.user;
 
 import com.eddicorp.application.controller.Controller;
+import com.eddicorp.http.request.BodyParser;
 import com.eddicorp.http.request.HttpRequest;
 import com.eddicorp.http.response.HttpResponse;
 
 import java.io.IOException;
+import java.util.Map;
 
 public class CreateUserController implements Controller {
     @Override
@@ -12,6 +14,12 @@ public class CreateUserController implements Controller {
         for (String key : request.getHeaderMap().keySet()) {
             System.out.println("key: " + key +"\n" + "value: " + request.getHeaderMap().get(key));
         }
-        System.out.println(request.getBody());
+
+        BodyParser bodyParser = new BodyParser(request.getBody());
+        Map<String, String> mappedBody = bodyParser.mappedBody;
+        for (String s : mappedBody.keySet()) {
+            System.out.println("key: " + s);
+            System.out.println("value: "+mappedBody.get(s));
+        }
     }
 }
--- src/main/java/com/eddicorp/http/request/BodyParser.java
+++ src/main/java/com/eddicorp/http/request/BodyParser.java
@@ -1,4 +1,21 @@
 package com.eddicorp.http.request;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class BodyParser {
+
+    public Map<String, String> mappedBody = new HashMap<>();
+
+    public Map<String, String> getMappedBody() {
+        return mappedBody;
+    }
+
+    public BodyParser(String body) {
+        String[] bodyArray = body.split("&");
+        for (String bodyData : bodyArray) {
+            String[] keyAndValue = bodyData.split("=");
+            this.mappedBody.put(keyAndValue[0], keyAndValue[1]);
+        }
+    }
 }
