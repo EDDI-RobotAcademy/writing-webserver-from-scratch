dbd51cd79aa3ed8c1a7d1eb63d0f9419e4172a13
ekankr2 <ekankr2@naver.com>
2023-03-21 23:29:41 +0900

    test: HttpResponseTest
    - setHeader(), setBody()

--- src/test/java/com/eddicorp/http/response/HttpResponseTest.java
+++ src/test/java/com/eddicorp/http/response/HttpResponseTest.java
@@ -31,13 +31,50 @@ class HttpResponseTest {
         String actual = outputStream.toString(StandardCharsets.UTF_8);
 
 //        String expectedHeaders = "Content-Type: application/json\r\n";
-//        String expectedCRLF = "\r\n";
 //        String expectedBody = "Test body";
 
 
         // then
         assertEquals(expected, actual);
-
     }
 
+    @DisplayName("setHeader(\"Content-Type\", \"application/json\")를 실행 시 Content-Type\", \"application/json를 header로 반환한다")
+    @Test
+    void test2() throws IOException {
+        // given
+        final ByteArrayOutputStream outputStream = new ByteArrayOutputStream(4096);
+        final HttpResponse sut = new HttpResponse(outputStream);
+        sut.setHttpStatus(HttpStatus.OK);
+        sut.setHeader("Content-Type", "application/json");
+//        sut.setBody("Test body".getBytes(StandardCharsets.UTF_8));
+
+        sut.flush();
+
+        // when
+        String actual = outputStream.toString(StandardCharsets.UTF_8);
+        String expected = "HTTP/1.1 200 OK\r\nContent-Type: application/json\r\n\r\n";
+//        String expectedBody = "Test body";
+
+        // then
+        assertEquals(expected, actual);
+    }
+
+    @DisplayName("setBody(\"Test body\")를 실행 시 Test body를 body로 반환한다")
+    @Test
+    void test3() throws IOException {
+        // given
+        final ByteArrayOutputStream outputStream = new ByteArrayOutputStream(4096);
+        final HttpResponse sut = new HttpResponse(outputStream);
+        sut.setHttpStatus(HttpStatus.OK);
+        sut.setBody("Test body".getBytes(StandardCharsets.UTF_8));
+
+        sut.flush();
+
+        // when
+        String expected = "HTTP/1.1 200 OK\r\n\r\nTest body";
+        String actual = outputStream.toString(StandardCharsets.UTF_8);
+
+        // then
+        assertEquals(expected, actual);
+    }
 }
